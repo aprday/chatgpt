8f2a9c01bc82d949d05dd1bdebbb7d4b05da5c3e
aprday <87921660+aprday@users.noreply.github.com>
2023-04-07 22:29:28 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/aprday/chatgpt/main/117905421.jpg',
+      name: '速播机器人',
+      description: 'ChatGPT 项目网站 <a href="https://www.supor.cn/" class="text-blue-500" target="_blank" >SUPOR</a>',
     },
   }
 }
